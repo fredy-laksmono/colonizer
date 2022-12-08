c2a6450b75fc897abd1507c0000e59d2b00dc8d6
Fredy Laksmono <fredy.laksmono@gmail.com>
2022-12-08 11:52:38 -0600

    Add: initial router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
 import "./styles/App.css";
+import { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,6 @@
+const Home = () => {
+  let toRender = <div>Hello</div>;
+  return toRender;
+};
+
+export default Home;
