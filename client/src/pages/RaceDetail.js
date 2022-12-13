384bfe404a2cfe4897932e4502e3d2886b65db1c
Fredy Laksmono <fredy.laksmono@gmail.com>
2022-12-12 19:36:04 -0600

    Add: Initial Race Detail page

--- client/src/pages/RaceDetail.js
+++ client/src/pages/RaceDetail.js
@@ -1,4 +1,21 @@
+import { useState } from "react";
 const RaceDetail = () => {
+  const [raceForm, updateRaceForm] = useState({
+    name: "",
+    grow: 1,
+    damage: 1,
+    health: 1,
+    uniqueAbility: 0
+  });
+  let toRender = (
+    <div>
+      <div>Create your first race</div>
+      <div>
+        <label>Name</label>
+        <input />
+      </div>
+    </div>
+  );
   return;
 };
 export default RaceDetail;
