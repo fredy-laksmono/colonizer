249b306dc0569d3f61d621b517d06c089055fff7
Fredy Laksmono <fredy.laksmono@gmail.com>
2022-12-14 09:51:22 -0600

    Attempt make frontend heroku use backend heroku 1.0

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import Axios from "axios";
 
 export const BASE_URL = "http://localhost:3001";
 
-const Client = Axios.create({ baseURL: BASE_URL });
+const Client = Axios.create();
 
 // Intercepts every request axios makes
 Client.interceptors.request.use(
