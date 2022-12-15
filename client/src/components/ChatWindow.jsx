e7ea2bde7b825fe95861d5c99320d3c9833916ee
Fredy Laksmono <fredy.laksmono@gmail.com>
2022-12-14 21:17:06 -0600

    Clean up chat window

--- client/src/components/ChatWindow.jsx
+++ client/src/components/ChatWindow.jsx
@@ -1,53 +1,34 @@
 import ChatCard from "./ChatCard"
 import { useState, useEffect } from "react"
+const uuid = require('uuid')
 
 const ChatWindow = ({socket, user, room}) => {
 
     const[chatInput, setChatInput] = useState("")
     const[chatList, setChatList] = useState([])
 
-    // let chats = [
-    //     {name: "Bob", message: "Hello"},
-    //     {name: "Alice", message: "Hi"},
-    //     {name: "Bob", message: "How are you?"},
-    //     {name: "Alice", message: "I'm good, how are you?"},
-    //     {name: "Bob", message: "I'm good, thanks"},
-    //     {name: "Alice", message: "That's good to hear"},
-    // ]
-
     const handleChatInput = (e) => {
         setChatInput(e.target.value)
     }
 
     const handleSend = () => {
-        console.log("sending", chatInput)
-        console.log("by", user.name)
-        console.log("in", room)
         socket.emit("chat_send", {message: chatInput, user: user.name, room: room})
         setChatInput("")
-        // socket.emit("chat_send",{chatInput,room})
     }
     
     useEffect(() => {
         socket.on("chat_recieve", (data) => {
-            console.log("received", data)
-            console.log("chatList", chatList)
             setChatList(chatList => [...chatList, {data}])
         })
-        // socket.on("receive_message", (data) => {
-        //     console.log("received", data)
-        //     // console.log("chatList", chatList)
-        //     // setChatList(chatList => [...chatList, {data}])
-        // })
     },[socket])
 
     let chatListRender = (<div>
         {chatList.map((chat)=>(
-                <ChatCard chat={chat} />
+                <ChatCard key={uuid.v4()} chat={chat} />
             ))}
     </div>)
     let chatInputRender = (<div>
-        <input onChange={handleChatInput} type="text" placeholder="Message" /><button onClick={handleSend}>Send</button>
+        <input onChange={handleChatInput} value={chatInput} type="text" placeholder="Message" /><button onClick={handleSend}>Send</button>
     </div>)
 
     let toRender = (<div>
